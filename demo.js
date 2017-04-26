4f8ddb02005990b42a045393b8897618599449fd
Emma Highland <ehighland@luc.edu>
2017-04-26 18:14:29 -0500

    better?

--- demo.js
+++ demo.js
@@ -1,22 +1,26 @@
 function addfave(x){
-	var article = ['Boyd, K. et al.\n"Finding Function in the Unknown." Proceedings - 2015 IEEE International Conference on Bioinformatics and Biomedicine, BIBM 2015.',
-	'Wheeler, H. et al.\n"Survey of the Heritability and Sparse Architecture of Gene Expression Traits across Human Tissues." Ed. Stephen B Montgomery. PLOS Genetics 12.11 (2016): e1006423.1',
-	'Zhou, Xiang et al.\n"Polygenic Modeling with Bayesian Sparse Linear Mixed Models." Ed. Peter M. Visscher. PLoS Genetics 9.2 (2013): e1003264. Web. 12 Oct. 2016.',
-	'The International Schizophrenia Consortium, et al.\n"Common Polygenic Variation Contributes to Risk of Schizophrenia and Bipolar Disorder." Nature 460 (2009).']
+	var article = ['Boyd, K. et al. "Finding Function in the Unknown." Proceedings - 2015 IEEE International Conference on Bioinformatics and Biomedicine, BIBM 2015.\n',
+	'Wheeler, H. et al. "Survey of the Heritability and Sparse Architecture of Gene Expression Traits across Human Tissues." Ed. Stephen B Montgomery. PLOS Genetics 12.11 (2016): e1006423.1\n',
+	'Zhou, Xiang et al. "Polygenic Modeling with Bayesian Sparse Linear Mixed Models." Ed. Peter M. Visscher. PLoS Genetics 9.2 (2013): e1003264. Web. 12 Oct. 2016.\n',
+	'The International Schizophrenia Consortium, et al."Common Polygenic Variation Contributes to Risk of Schizophrenia and Bipolar Disorder." Nature 460 (2009).\n']
 	
 	var faves = []
 	
 	if (x == "Boyd"){
-		faves.push(article[0]);
+		article = article[0];
+		faves.concat(article);
 	}
 	else if (x == "Wheeler"){
-		faves.push(article[1]);
+		article = article[1];
+		faves.concat(article);
 	}
 	else if (x == "Zhou"){
-		faves.push(article[2]);
+		article = article[2];
+		faves.concat(article);
 	}
 	else if (x == "ISC"){
-		faves.push(article[3]);
+		article = article[3];
+		faves.concat(article);
 	}
 	
 	console.log(faves);
